c33ba203186249ea2d6e943b068b260081f8d999
Daniel Olsen <dolsen@csus.edu>
2022-04-19 17:18:58 -0700

    added base functionality

---
+++ script.js
@@ -0,0 +1,19 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function slideChange(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  console.log("here");
+  let i;
+  let slides = document.getElementsByClassName("project");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  console.log("made it here");
+}
