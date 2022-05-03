bf2933ce3140e3f4a4e6b6b34bbe7d6b4e09af0d
Daniel Olsen <dolsen@csus.edu>
2022-05-03 01:59:16 -0700

    changed titles for animation. added dots

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ function slideChange(n) {
   showSlides(slideIndex += n);
 }
 
+function changeSlideTo(n){
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
   console.log("here");
   let i;
   let slides = document.getElementsByClassName("project");
+  let slideshowDots = document.getElementsByClassName("slideshow-dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  for (i = 0; i < slideshowDots.length; i++) {
+    slideshowDots[i].className = slideshowDots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "flex";
-  console.log("made it here");
+  slideshowDots[slideIndex-1].className += " active";
 }
